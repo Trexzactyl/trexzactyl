53b1a1d93fbc7df9c107d7184d5700b29700d167
ryanwtf88 <ryanwtf88@gmail.com>
2026-02-06 18:15:18 +0600

    Replace styled-component with forwardRef div in SubNavigation to fix hooks warnings

--- resources/scripts/components/elements/SubNavigation.tsx
+++ resources/scripts/components/elements/SubNavigation.tsx
@@ -1,31 +1,57 @@
+import React from 'react';
 import tw, { theme } from 'twin.macro';
-import styled from 'styled-components/macro';
 
-const SubNavigation = styled.div`
-    ${tw`bg-neutral-900 bg-opacity-40 backdrop-blur-xl border-b border-white/5 overflow-x-auto mb-4 sm:mb-10 w-full`};
-
-    & > div {
-        ${tw`flex text-sm mx-auto px-2 max-w-7xl`};
-
-        & > a,
-        & > div {
-            ${tw`inline-block py-3 px-4 text-neutral-400 no-underline whitespace-nowrap transition-all duration-300 font-bold text-xs`};
-
-            &:not(:first-of-type) {
-                ${tw`ml-2`};
+const SubNavigation = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>((props, ref) => (
+    <div
+        ref={ref}
+        {...props}
+        css={[
+            tw`bg-neutral-900 bg-opacity-40 backdrop-blur-xl border-b border-white/5 overflow-x-auto mb-4 sm:mb-10 w-full`,
+            `
+            & > div {
+                display: flex;
+                font-size: 0.875rem;
+                margin-left: auto;
+                margin-right: auto;
+                padding-left: 0.5rem;
+                padding-right: 0.5rem;
+                max-width: 80rem;
             }
 
-            &:hover {
-                ${tw`text-neutral-100`};
+            & > div > a,
+            & > div > div {
+                display: inline-block;
+                padding: 0.75rem 1rem;
+                color: rgb(163 163 163);
+                text-decoration: none;
+                white-space: nowrap;
+                transition: all 300ms;
+                font-weight: 700;
+                font-size: 0.75rem;
             }
 
-            &:active,
-            &.active {
-                ${tw`text-blue-400`};
+            & > div > a:not(:first-of-type),
+            & > div > div:not(:first-of-type) {
+                margin-left: 0.5rem;
+            }
+
+            & > div > a:hover,
+            & > div > div:hover {
+                color: rgb(245 245 245);
+            }
+
+            & > div > a:active,
+            & > div > a.active,
+            & > div > div:active,
+            & > div > div.active {
+                color: rgb(96 165 250);
                 box-shadow: inset 0 -2px ${theme`colors.blue.500`.toString()};
             }
-        }
-    }
-`;
+            `,
+        ]}
+    />
+));
+
+SubNavigation.displayName = 'SubNavigation';
 
 export default SubNavigation;
