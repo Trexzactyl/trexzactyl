2999fc227416e0d5d01b09b4513c9bf380d4e6d0
ryanwtf88 <ryanwtf88@gmail.com>
2026-02-06 17:56:56 +0600

    Replace styled-component with plain div in Fade to fix hooks order

--- resources/scripts/components/elements/Fade.tsx
+++ resources/scripts/components/elements/Fade.tsx
@@ -1,49 +1,53 @@
 import React from 'react';
 import tw from 'twin.macro';
-import styled from 'styled-components/macro';
 import CSSTransition, { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 
 interface Props extends Omit<CSSTransitionProps, 'timeout' | 'classNames'> {
     timeout: number;
 }
 
-const Container = styled.div<{ timeout: number }>`
-    .fade-enter,
-    .fade-exit,
-    .fade-appear {
-        will-change: opacity;
-    }
-
-    .fade-enter,
-    .fade-appear {
-        ${tw`opacity-0`};
-
-        &.fade-enter-active,
-        &.fade-appear-active {
-            ${tw`opacity-100 transition-opacity ease-in`};
-            transition-duration: ${(props) => props.timeout}ms;
-        }
-    }
-
-    .fade-exit {
-        ${tw`opacity-100`};
-
-        &.fade-exit-active {
-            ${tw`opacity-0 transition-opacity ease-in`};
-            transition-duration: ${(props) => props.timeout}ms;
-        }
-    }
-`;
-
 const Fade: React.FC<Props> = ({ timeout, children, ...props }) => {
     const nodeRef = React.useRef<HTMLDivElement>(null);
     
     return (
-        <Container timeout={timeout}>
+        <div
+            css={[
+                tw``,
+                `
+                .fade-enter,
+                .fade-exit,
+                .fade-appear {
+                    will-change: opacity;
+                }
+
+                .fade-enter,
+                .fade-appear {
+                    opacity: 0;
+                }
+
+                .fade-enter-active,
+                .fade-appear-active {
+                    opacity: 1;
+                    transition: opacity ease-in;
+                    transition-duration: ${timeout}ms;
+                }
+
+                .fade-exit {
+                    opacity: 1;
+                }
+
+                .fade-exit-active {
+                    opacity: 0;
+                    transition: opacity ease-in;
+                    transition-duration: ${timeout}ms;
+                }
+                `
+            ]}
+        >
             <CSSTransition timeout={timeout} classNames={'fade'} {...props} nodeRef={nodeRef}>
                 <div ref={nodeRef}>{children}</div>
             </CSSTransition>
-        </Container>
+        </div>
     );
 };
 Fade.displayName = 'Fade';
